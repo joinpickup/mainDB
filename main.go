28ae3944a883cd6c0bb760351e276e919d3e2c5e
andrew cohen <cohen.andrew.n@gmail.com>
2022-07-14 00:42:01 -0400

    remove token

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	m, err := migrate.New("github://andyrewco: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@joinpickup/maindb/db/migrations", "postgresql://doadmin:AVNS_MxlmIZnOFaKU6E_nDvS@private-pickup-do-user-11994297-0.b.db.ondigitalocean.com:25060/maindb?sslmode=require")
+	m, err := migrate.New("github://joinpickup/maindb/db/migrations", "postgresql://doadmin:AVNS_MxlmIZnOFaKU6E_nDvS@private-pickup-do-user-11994297-0.b.db.ondigitalocean.com:25060/maindb?sslmode=require")
 	if err != nil {
 		log.Fatal(err)
 	}
