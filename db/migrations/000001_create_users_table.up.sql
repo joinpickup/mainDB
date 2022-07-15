a2a50cf38724457aac729fbf983fc531eae10e51
andrew cohen <cohen.andrew.n@gmail.com>
2022-07-15 17:48:46 -0400

    make username not null

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 create table if not exists "user" (
   "id" serial primary key,
-  "username" varchar (50) unique not null,
+  "username" varchar (50) unique,
   "email" varchar (50) unique not null,
   "password_hash" varchar not null
 )
