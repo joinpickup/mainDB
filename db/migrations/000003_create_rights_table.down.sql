9f9ed092754f07414d59d5b1f7d11e33ffa81f34
andrew cohen <cohen.andrew.n@gmail.com>
2022-07-15 12:48:42 -0400

    add roles and rights table

---
+++ db/migrations/000003_create_rights_table.down.sql
@@ -0,0 +1,4 @@
+drop table if exists "user_role";
+drop table if exists "role_right";
+drop table if exists "right";
+drop table if exists "role";
---
+++ db/migrations/000003_create_rights_table.up.sql
@@ -0,0 +1,25 @@
+create table if not exists "right" (
+  "id" serial primary key,
+  "name" varchar (70) unique not null,
+  "description" varchar (254)
+)
+
+create table if not exists "role" (
+  "id" serial primary key,
+  "name" varchar (70) unique not null,
+  "description" varchar (254)
+)
+
+create table if not exists "role_right" (
+  "role_id" int,
+  "right_id" int,
+  "enabled" boolean,
+  pimary key(role_id, right_id)
+)
+
+create table if not exists "user_role" (
+  "role_id" int,
+  "user_id" int,
+  "enabled" boolean,
+  primary key(user_id, role_id)
+)
